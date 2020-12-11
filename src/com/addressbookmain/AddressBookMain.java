33c7b3cb3b5698021b1d320e14a602e017bd249c
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-11 09:15:56 +0530

    Added: Person Class & Its Elements

--- src/com/addressbookmain/AddressBookMain.java
+++ src/com/addressbookmain/AddressBookMain.java
@@ -1,6 +1,28 @@
 
 package com.addressbookmain;
 
+class Person 
+{   
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+ 
+    public Person(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }    
+}
+
 public class AddressBookMain 
 {
     public static void main(String[] args) 
