bec8e5929c38281f28945c4d44cb67880ff19c13
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-11 09:07:13 +0530

    Added: Welcome To Address Book Message

---
+++ src/com/addressbookmain/AddressBookMain.java
@@ -0,0 +1,10 @@
+
+package com.addressbookmain;
+
+public class AddressBookMain 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("*-*-* WELCOME TO ADDRESS BOOK *-*-*");
+    }  
+}
