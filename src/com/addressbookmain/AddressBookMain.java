4bd986b51640f0f1abcd84074e47abc2f5b9ba45
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-11 09:24:21 +0530

    Added: New Person

--- src/com/addressbookmain/AddressBookMain.java
+++ src/com/addressbookmain/AddressBookMain.java
@@ -1,6 +1,9 @@
 
 package com.addressbookmain;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Person 
 {   
     String firstName;
@@ -28,5 +31,27 @@ public class AddressBookMain
     public static void main(String[] args) 
     {
         System.out.println("*-*-* WELCOME TO ADDRESS BOOK *-*-*");
+        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Person> personList = new ArrayList<Person>();
+        
+        System.out.print("Enter first name:");
+        String firstName = sc.next();             
+        System.out.print("Enter last name:");
+        String lastName = sc.next();
+        System.out.print("Enter address:");
+        String address = sc.next();
+        System.out.print("Enter city:");
+        String city = sc.next();
+        System.out.print("Enter state:");
+        String state = sc.next();
+        System.out.print("Enter zip:");
+        String zip = sc.next();
+        System.out.print("Enter phoennumber:");
+        String phoneNumber = sc.next();
+                       
+        Person personObject = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        personList.add(personObject);
+        System.out.println("---Created Address Book and Inserted succesfully---");
     }  
 }
